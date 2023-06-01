7fd17c6ba340a0f3b13b3e50ad3aa7f993b27745
krafairus <64279814+krafairus@users.noreply.github.com>
2023-06-01 17:55:21 -0500

    Add files via upload

--- propiedades/js/custom.js
+++ propiedades/js/custom.js
@@ -1,21 +1,6 @@
 $(document).ready(function(){
 	"use strict";
     
-        /*==================================
-* Author        : "ThemeSine"
-* Template Name : Furniture E- commarce HTML Template
-* Version       : 1.0
-==================================== */
-
-
-
-
-/*=========== TABLE OF CONTENTS ===========
-1. Scroll To Top 
-2. owl carousel
-3. welcome animation support
-4. cart close
-======================================*/
 
     // 1. Scroll To Top 
 		$(window).on('scroll',function () {
